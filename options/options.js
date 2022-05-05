d1e0d883351860a9cf8512922d827d3d96130d3e
Crowdbotics <team@crowdbotics.com>
2022-05-05 15:31:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "carolina_closing_gi_35093",
+  url: "https://carolina_closing_gi_35093.botics.co",
+  api: "https://carolina_closing_gi_35093.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
